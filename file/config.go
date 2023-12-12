ab54f70b3811e7055db627089f94133c1cee706e
Keefer Taylor | Tessellated <keefer@tessellated.io>
2023-12-11 16:12:31 -0800

    revise router

--- file/config.go
+++ file/config.go
@@ -1,8 +1,11 @@
 package file
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/tessellated-io/pickaxe/config"
+	"github.com/tessellated-io/pickaxe/log"
 	"gopkg.in/yaml.v2"
 )
 
@@ -29,4 +32,33 @@ func parseConfig(filename string) (*Config, error) {
 	}
 
 	return config, nil
+}
+
+// Write an example config file.
+func InitializeConfigFile(filename string, configurationDirectory string, logger *log.Logger) error {
+	networks := []NetworkConfig{
+		NetworkConfig{
+			ChainID:      "cosmoshub-4",
+			GrpcEndpoint: "tcp://cosmos.rpc.directory",
+		},
+		NetworkConfig{
+			ChainID:      "osmosis-1",
+			GrpcEndpoint: "tcp://osmosis.rpc.directory",
+		},
+	}
+
+	routerConfig := Config{
+		Networks: networks,
+	}
+
+	// Create folder if needed
+	config.CreateDirectoryIfNeeded(configurationDirectory, logger)
+
+	routerConfigFile := fmt.Sprintf("%s/%s", configurationDirectory, filename)
+	header := "This is a file to route chain IDs to RPC endpoints\n"
+	err := config.WriteYamlWithComments(routerConfig, header, routerConfigFile, logger)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,13 @@ module github.com/tessellated-io/router
 go 1.20
 
 require (
-	github.com/tessellated-io/pickaxe v1.0.11
+	github.com/tessellated-io/pickaxe v1.0.14
 	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/rs/zerolog v1.29.1 // indirect
+	golang.org/x/sys v0.10.0 // indirect
 )
